e49f190b09ea45371183a15b38d91eebae462930
gupta-surabhi <48524493+gupta-surabhi@users.noreply.github.com>
2022-08-18 15:32:01 +0530

    Update LogAnalysisServiceApplication.java

--- src/main/java/com/test/loganalysisservices/LogAnalysisServiceApplication.java
+++ src/main/java/com/test/loganalysisservices/LogAnalysisServiceApplication.java
@@ -32,5 +32,34 @@ public class LogAnalysisServiceApplication {
 						 * "sur.gupta93@gmail.com")) .build())
 						 */;
 	}
+	
+	/*
+	mvc:
+    pathmatch:
+      matching-strategy: ant-path-matcher
+	@Bean
+	public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier,
+	        ServletEndpointsSupplier servletEndpointsSupplier, ControllerEndpointsSupplier controllerEndpointsSupplier,
+	        EndpointMediaTypes endpointMediaTypes, CorsEndpointProperties corsProperties,
+	        WebEndpointProperties webEndpointProperties, Environment environment) {
+	    List<ExposableEndpoint<?>> allEndpoints = new ArrayList();
+	    Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+	    allEndpoints.addAll(webEndpoints);
+	    allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+	    allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+	    String basePath = webEndpointProperties.getBasePath();
+	    EndpointMapping endpointMapping = new EndpointMapping(basePath);
+	    boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment,
+	            basePath);
+	    return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes,
+	            corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath),
+	            shouldRegisterLinksMapping, null);
+	}
+
+	private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment,
+	        String basePath) {
+	    return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath)
+	        || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
+	} */
 
 }
