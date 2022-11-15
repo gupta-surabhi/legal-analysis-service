9dfe30e0e725bef15af7c0965f5e933f012794ea
gupta-surabhi <48524493+gupta-surabhi@users.noreply.github.com>
2022-11-15 20:09:24 +0530

    Update EventDataController.java

--- src/main/java/com/test/loganalysisservices/controllers/EventDataController.java
+++ src/main/java/com/test/loganalysisservices/controllers/EventDataController.java
@@ -73,4 +73,29 @@ springdoc.api-docs.path=/api-docs
 		return ResponseEntity.ok(eventData);
 
 	}
+	
+	/*
+	io.swagger.v3.oas.annotations.
+
+@Api → @Tag
+
+@ApiIgnore → @Parameter(hidden = true) or @Operation(hidden = true) or @Hidden
+
+@ApiImplicitParam → @Parameter
+
+@ApiImplicitParams → @Parameters
+
+@ApiModel → @Schema
+
+@ApiModelProperty(hidden = true) → @Schema(accessMode = READ_ONLY)
+
+@ApiModelProperty → @Schema
+
+@ApiOperation(value = "foo", notes = "bar") → @Operation(summary = "foo", description = "bar")
+
+@ApiParam → @Parameter
+
+@ApiResponse(code = 404, message = "foo") → @ApiResponse(responseCode = "404", description = "foo")
+	
+	*/
 }
